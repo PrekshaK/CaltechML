773a4768b4ef1f945a1bb31288e9151c4b06266d
Preksha Koirala <prekshakoirala@gmail.com>
2016-04-23 01:26:22 -0400

    changed presentation and doc and code little bit

--- project/regression.py
+++ project/regression.py
@@ -4,32 +4,43 @@ import sklearn as sk
 from sklearn import tree
 from sklearn.linear_model import LogisticRegression
 
-import os
 
+#importing dataset
 dataset= np.genfromtxt('parole.csv', delimiter=',')
+
+#importing test dataset
 testdataset = np.genfromtxt('paroletest.csv', delimiter=",")
 
+#x and y dataset for test
 y = testdataset[:, -1]
 x = testdataset[:, [0, -3]]
 
+#x and y for training
 X = dataset[:,[0, -3]]
 Y = dataset[:, -1]
+
+#printing y to see the required prediction
 print y
 
+#Using decision tree to predict the data
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, Y)
+clf = clf.fit(X, Y)  #Fitting data or training given dataset
 print clf.predict(x)
 
+#Using logistic regression to predict the data
 logreg = LogisticRegression()
-
-logreg.fit(X, Y)
+logreg.fit(X, Y) #Fitting data or training given dataset
 predicted =  logreg.predict(x)
+
+#Printing the predicted data
 print predicted
 
+#Assigning expected to the y of testdataset
 expected = y
 
+#Checking the accuracy of prediction
 summarize = sk.metrics.classification_report(expected, predicted)
+
+#Printing the result
 print summarize
 
-
-#71.45
